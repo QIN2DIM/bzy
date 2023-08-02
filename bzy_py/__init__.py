01ffb6c8bf274ea7a9fd811adb0aa513499662e3
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-08-03 00:11:17 +0800

    Delete bzypy

--- bzy_py/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Time       : 2023/8/2 23:04
-# Author     : QIN2DIM
-# Github     : https://github.com/QIN2DIM
-# Description:
--- bzy_py/main.py
+++
@@ -1,5 +0,0 @@
-# Author: BeiYu
-# Github: https://github.com/beiyuouo
-# Date  : 2021/2/22 16:19
-# Description:
-__author__ = "BeiYu"
