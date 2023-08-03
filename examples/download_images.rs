dbc3ce7154274d0ae2228f8df0b55087eee9d45e
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-08-03 22:38:23 +0800

    vv

--- examples/download_images.rs
+++ examples/download_images.rs
@@ -1,10 +1,61 @@
+use std::fs;
+use std::path::PathBuf;
+
 use anyhow::{Ok, Result};
-use log::{debug, error, info};
+use chrono::prelude::*;
+use log::{error, info};
+use rand::seq::SliceRandom;
+use reqwest::Client;
+use tokio::time::Instant;
 
 use bzy_rs::project::Project;
 
-async fn download_images(project: Project) -> Result<()> {
-    debug!("{:#?}", project);
+const BATCH_SIZE: usize = 10;
+
+async fn run_collector(project: &Project) -> Result<()> {
+    let mut urls = project.load_bzy_index()?;
+    info!("读入 {} 条待处理链接", urls.len());
+
+    info!("正在下载随机样本 - batch={BATCH_SIZE}");
+
+    // 创建样本缓存目录
+    let dt = Local::now().format("%Y-%m-%d %H%M%S").to_string();
+    let save_path = project.img_dir.join(dt);
+    if !save_path.exists() {
+        fs::create_dir_all(&save_path)?;
+    }
+
+    // 打乱任务队列
+    urls.shuffle(&mut rand::thread_rng());
+
+    // 初始化协程池
+    let client = Client::new();
+    let mut handles = Vec::new();
+
+    // 启动任务
+    for i in 1..BATCH_SIZE + 1 {
+        let url = &urls[i];
+        let handle = tokio::spawn(download_images(
+            client.clone(),
+            url.clone(),
+            save_path.clone(),
+        ));
+        handles.push(handle)
+    }
+    for handle in handles {
+        handle.await??;
+    }
+
+    Ok(())
+}
+
+async fn download_images(client: Client, url: String, save_path: PathBuf) -> Result<()> {
+    let img = client.get(&url).send().await?.bytes().await?;
+    let filename = url.split("/").last().unwrap().to_string();
+    let p = save_path.join(&filename);
+    fs::write(p, img)?;
+    info!("下载成功 - filename={}", &filename);
+
     Ok(())
 }
 
@@ -17,9 +68,10 @@ async fn main() -> Result<()> {
         project.pull_bzy_index().await?;
     };
 
-    download_images(project).await.unwrap_or_else(|err| {
-        error!("Opps~ {err}");
-    });
-
+    let start = Instant::now();
+    run_collector(&project)
+        .await
+        .unwrap_or_else(|err| error!("Opps~ {err}"));
+    info!("任务结束 - Time elapsed: {:?}", start.elapsed());
     Ok(())
 }
--- src/project.rs
+++ src/project.rs
@@ -1,12 +1,15 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
 use std::{fs, process};
 
 use anyhow::{Ok, Result};
 use env_logger;
 use log::LevelFilter;
-use reqwest::{Client, Proxy};
+use reqwest::Client;
 
-const HTTPS_PROXY: &str = "http://127.0.0.1:7890";
+const URL_TASKS: &str =
+    "https://dl.capoo.xyz/https://github.com/QIN2DIM/bzy-rs/releases/download/bzy-db/BenZiYunMining.txt";
 
 #[derive(Debug, Default)]
 pub struct Project {
@@ -33,10 +36,8 @@ impl Project {
     }
 
     pub async fn pull_bzy_index(&self) -> Result<()> {
-        let url = "https://github.com/QIN2DIM/bzy-rs/releases/download/bzy-db/BenZiYunMining.txt";
-
+        let url = URL_TASKS;
         let client = Client::builder()
-            .proxy(Proxy::all(HTTPS_PROXY)?)
             .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188")
             .build()?;
 
@@ -45,5 +46,13 @@ impl Project {
         fs::write(&self.bzy_index, bytes_db)?;
 
         Ok(())
+    }
+
+    pub fn load_bzy_index(&self) -> Result<Vec<String>> {
+        let file = File::open(&self.bzy_index)?;
+        let reader = BufReader::new(file);
+        let urls = reader.lines().map(|l| l.unwrap()).collect();
+
+        Ok(urls)
     }
 }
